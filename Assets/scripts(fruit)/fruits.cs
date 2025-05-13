b1ab619bd3144d1f7b59c136084427809772cab4
ngjsm <ng38905@gmail.com>
2025-05-13 23:32:41 +0900

    레포수정본

--- Assets/scripts(fruit)/fruits.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum FruitType { apple, banana, orange }
-public class fruit : MonoBehaviour {
-    public FruitType fruitType;
-}
